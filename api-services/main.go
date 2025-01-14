8e5be0435cb366072cbc7481d007cda4148995d8
shivam-sonkerr <shivam87380@gmail.com>
2025-01-14 08:32:25 +0530

    POST handler defined to get a URL and save it in-memory

--- api-services/main.go
+++ api-services/main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
+
+type longURL struct {
+	OriginalUrl string `json:"originalUrl"`
+}
 
 func handlerPing(c *gin.Context) {
 
 	c.JSON(200, gin.H{"message": "pong"})
+}
+
+func urlPOST(c *gin.Context) {
+	var originalUrl longURL
+
+	if err := c.BindJSON(&originalUrl); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+	fmt.Println("Received URL:", originalUrl.OriginalUrl)
+
+	return
 }
 
 func main() {
@@ -14,6 +31,8 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/ping", handlerPing)
+
+	r.POST("/shorten", urlPOST)
 
 	r.Run("localhost:8080")
 
