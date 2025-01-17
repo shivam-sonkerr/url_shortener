8cc85a596b6391479f889251e06e6d34ae6f4075
shivam-sonkerr <shivam87380@gmail.com>
2025-01-17 13:32:45 +0530

    Created a new folder named "handlers" to keep all the API handlers together and make the call from main whenever needed.

--- api-services/main.go
+++ api-services/main.go
@@ -1,192 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	"log"
-	"math/rand/v2"
-	"net/http"
-	"net/url"
 	"url_shortener/dbutils"
-	"url_shortener/models"
+	"url_shortener/handlers"
 )
-
-func handlerPing(c *gin.Context) {
-
-	c.JSON(200, gin.H{"message": "pong"})
-}
-
-func redirectURLHandler(c *gin.Context) {
-
-	db, err := dbutils.ConnectDB()
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to connect to the database"})
-		return
-	}
-	defer db.Close() // Close the database connection after the handler completes
-
-	shortURL := c.Param("shortURL")
-
-	fmt.Println("Redirecting for shortURL:", shortURL) // log the shortURL to confirm
-
-	originalURL, err := dbutils.CheckIfURLExists(db, shortURL)
-
-	//if err != nil {
-	//	c.JSON(http.StatusNotFound, gin.H{"error": "NOT FOUND"})
-	//	return
-	//}
-
-	if originalURL == "" {
-		c.JSON(http.StatusNotFound, gin.H{"error": "shortURL has no valid mapping"})
-		return
-	}
-
-	c.Redirect(http.StatusMovedPermanently, originalURL)
-}
-
-func isValidURL(u string) bool {
-	_, err := url.ParseRequestURI(u)
-	return err == nil
-}
-
-func urlPOST(c *gin.Context) {
-	var request models.URLMappings
-
-	if err := c.BindJSON(&request); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	}
-
-	// Validate the Original URL
-	if !isValidURL(request.OriginalURL) {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid URL format"})
-		return
-	}
-
-	//checking if the provided URL is not empty
-
-	if request.OriginalURL == "" {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Original URL cannot be empty"})
-		return
-	}
-
-	//connecting to the database
-	db, err := dbutils.ConnectDB()
-
-	//check to ensure that database connectivity is working fine.
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to connect to database."})
-	}
-	defer db.Close()
-
-	var shortURL string
-
-	for {
-		shortURL = urlGenerator()
-		var exists int
-
-		err = db.QueryRow("SELECT COUNT(*) FROM url_mappings where short_url = ?", shortURL).Scan(&exists)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to validate uniqueness of short URL"})
-		}
-		if exists == 0 {
-			break
-		}
-	}
-
-	_, err = dbutils.AddURL(db, request.OriginalURL, shortURL)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save URL mapping"})
-	}
-
-	c.JSON(http.StatusCreated, gin.H{
-		"message":  "Short URL created successfully",
-		"shortURL": shortURL,
-	})
-
-	fmt.Printf("New URL mapping added: %s -> %s\n", request.OriginalURL, shortURL)
-
-	c.Redirect(http.StatusMovedPermanently, fmt.Sprintf("http://localhost:8080/redirect/%s", shortURL))
-	//fmt.Println("Received URL:", originalUrl.OriginalURL)
-	return
-}
-
-func urlGenerator() string {
-	random := rand.IntN(9000) + 1000
-
-	prefix := "simple-url"
-	fmt.Println(random)
-
-	shortURL := fmt.Sprintf("%s-%d", prefix, random)
-	return shortURL
-}
-
-//func initDB(db *sql.DB) error {
-//	query := `
-//       CREATE TABLE IF NOT EXISTS url_mappings (
-//           id BIGINT AUTO_INCREMENT PRIMARY KEY,
-//           original_url TEXT NOT NULL,
-//           short_url VARCHAR(255) NOT NULL UNIQUE,
-//           created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-//       );
-//   `
-//	_, err := db.Exec(query)
-//	return err
-//}
-
-func shortenAndRedirect(c *gin.Context) {
-	var request models.URLMappings
-
-	// Parse the request body
-	if err := c.BindJSON(&request); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request body"})
-		return
-	}
-
-	// Validate the provided URL
-	if request.OriginalURL == "" || !isValidURL(request.OriginalURL) {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid or empty URL"})
-		return
-	}
-
-	// Connect to the database
-	db, err := dbutils.ConnectDB()
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to connect to the database"})
-		return
-	}
-	defer db.Close()
-
-	// Generate a unique short URL
-	var shortURL string
-	for {
-		shortURL = urlGenerator()
-		var exists int
-		err = db.QueryRow("SELECT COUNT(*) FROM url_mappings WHERE short_url = ?", shortURL).Scan(&exists)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Database error"})
-			return
-		}
-		if exists == 0 {
-			break
-		}
-	}
-
-	// Save the URL mapping in the database
-	//newMapping := models.URLMappings{
-	//	OriginalURL: request.OriginalURL,
-	//	ShortURL:    shortURL,
-	//	CreatedAt:   time.Now(),
-	//}
-	_, err = dbutils.AddURL(db, request.OriginalURL, shortURL)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save URL mapping"})
-		return
-	}
-
-	// Redirect the user to the original URL
-	c.Redirect(http.StatusMovedPermanently, request.OriginalURL)
-}
 
 func main() {
 
@@ -204,13 +24,11 @@ func main() {
 
 	r := gin.Default()
 
-	r.GET("/ping", handlerPing)
+	r.GET("/ping", handlers.HandlerPing)
 
-	r.POST("/shorten", urlPOST)
+	r.POST("/shorten", handlers.URLPost)
 
-	r.POST("/shorten-and-redirect", shortenAndRedirect)
-
-	r.GET("/redirect/:shortURL", redirectURLHandler)
+	r.POST("/shorten-and-redirect", handlers.ShortenAndRedirect)
 
 	r.Run("localhost:8080")
 
---
+++ handlers/handlers.go
@@ -0,0 +1,139 @@
+package handlers
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"math/rand/v2"
+	"net/http"
+	"net/url"
+	"url_shortener/dbutils"
+	"url_shortener/models"
+)
+
+// Ping Handler
+func HandlerPing(c *gin.Context) {
+	c.JSON(200, gin.H{"message": "pong"})
+}
+
+// URL Redirect Handler
+func RedirectURLHandler(c *gin.Context) {
+	db, err := dbutils.ConnectDB()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to connect to the database"})
+		return
+	}
+	defer db.Close()
+
+	shortURL := c.Param("shortURL")
+	originalURL, err := dbutils.CheckIfURLExists(db, shortURL)
+
+	if originalURL == "" {
+		c.JSON(http.StatusNotFound, gin.H{"error": "shortURL has no valid mapping"})
+		return
+	}
+
+	c.Redirect(http.StatusMovedPermanently, originalURL)
+}
+
+// URL Post Handler
+func URLPost(c *gin.Context) {
+	var request models.URLMappings
+	if err := c.BindJSON(&request); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if !isValidURL(request.OriginalURL) {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid URL format"})
+		return
+	}
+
+	db, err := dbutils.ConnectDB()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to connect to database."})
+		return
+	}
+	defer db.Close()
+
+	var shortURL string
+	for {
+		shortURL = urlGenerator()
+		var exists int
+		err = db.QueryRow("SELECT COUNT(*) FROM url_mappings where short_url = ?", shortURL).Scan(&exists)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to validate uniqueness of short URL"})
+			return
+		}
+		if exists == 0 {
+			break
+		}
+	}
+
+	_, err = dbutils.AddURL(db, request.OriginalURL, shortURL)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save URL mapping"})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"message":  "Short URL created successfully",
+		"shortURL": shortURL,
+	})
+	c.Redirect(http.StatusMovedPermanently, fmt.Sprintf("http://localhost:8080/redirect/%s", shortURL))
+}
+
+// Shorten and Redirect Handler
+func ShortenAndRedirect(c *gin.Context) {
+	var request models.URLMappings
+	if err := c.BindJSON(&request); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request body"})
+		return
+	}
+
+	if request.OriginalURL == "" || !isValidURL(request.OriginalURL) {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid or empty URL"})
+		return
+	}
+
+	db, err := dbutils.ConnectDB()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to connect to the database"})
+		return
+	}
+	defer db.Close()
+
+	var shortURL string
+	for {
+		shortURL = urlGenerator()
+		var exists int
+		err = db.QueryRow("SELECT COUNT(*) FROM url_mappings WHERE short_url = ?", shortURL).Scan(&exists)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Database error"})
+			return
+		}
+		if exists == 0 {
+			break
+		}
+	}
+
+	_, err = dbutils.AddURL(db, request.OriginalURL, shortURL)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save URL mapping"})
+		return
+	}
+
+	c.Redirect(http.StatusMovedPermanently, request.OriginalURL)
+}
+
+// Helper Functions
+func isValidURL(u string) bool {
+	_, err := url.ParseRequestURI(u)
+	return err == nil
+}
+
+func urlGenerator() string {
+	random := rand.IntN(9000) + 1000
+	prefix := "simple-url"
+	shortURL := fmt.Sprintf("%s-%d", prefix, random)
+	return shortURL
+}
